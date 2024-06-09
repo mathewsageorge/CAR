85a4da7689d81e56dbd290cd0822706e12c9995f
mathewsageorge <mathewsgeorge202@gmail.com>
2024-06-09 13:51:03 +0530

    first comm

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,19 +142,19 @@ const initAccordion = function (currentAccordion) {
 
 }
 
-for (let i = 0, len = accordions.length; i < len; i++) { initAccordion(accordions[i]); }
+// for (let i = 0, len = accordions.length; i < len; i++) { initAccordion(accordions[i]); }
 
-let slideIndex = 0;
-  showSlides();
+// let slideIndex = 0;
+//   showSlides();
 
-  function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("slider-item");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 5000); // Change slide every 2 seconds
-  }
+//   function showSlides() {
+//     let i;
+//     let slides = document.getElementsByClassName("slider-item");
+//     for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}
+//     slides[slideIndex-1].style.display = "block";
+//     setTimeout(showSlides, 5000); // Change slide every 2 seconds
+//   }
